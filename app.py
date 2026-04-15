b09c3026a0cb6253f24efd111e7d6902fa36c145
Lalithambigai <lalithambigai8449@gmail.com>
2026-04-15 23:51:16 +0800

    modified app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request ,redirect
 
 app = Flask(__name__)
 
@@ -14,9 +14,19 @@ def login():
 def logout():
     return 'Logout page - coming soon'
 
+tasks_list = []
 @app.route('/tasks')
 def tasks():
-    return render_template("tasks.html")
+    return render_template("tasks.html",tasks=tasks_list)
+
+@app.route('/add', methods=['POST','GET'])
+def add():
+    if request.method =='POST':
+        tasks = request.form['tasks']
+        tasks_list.append(tasks)
+        return redirect('/tasks')
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
