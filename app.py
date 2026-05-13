c67a644fdaaebcb98d5a97c61ed7a570204ab187
Ratna Jane <ratnajane0207@gmail.com>
2026-05-14 00:29:53 +0800

    Fixed redirection

--- app.py
+++ app.py
@@ -230,7 +230,7 @@ def dashboard():
 @app.route("/logout")
 def logout():
     session.clear()
-    return redirect(url_for("login"))
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
